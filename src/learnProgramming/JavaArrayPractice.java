fe937fe8bcfc430e2f3a58d90376f704d2186d25
poornima S <poornii87@gmail.com>
2019-09-21 12:04:02 +0100

    first commit

---
+++ src/learnProgramming/JavaArrayPractice.java
@@ -0,0 +1,12 @@
+package learnProgramming;
+
+public class JavaArrayPractice {
+    public static void main(String[] args) {
+        String[] cars = { "Volvo","BMW","Ford","Maza"};
+
+
+
+    }
+}
+
+
